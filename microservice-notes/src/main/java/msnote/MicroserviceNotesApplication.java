2c80568956f3c3d84e5b29e846fa4947c37ceeb4
walid938 <w.seghdau@gmail.com>
2024-06-05 19:07:18 +0200

    Initialize notes microservice project with Spring Boot

---
+++ microservice-notes/src/main/java/msnote/MicroserviceNotesApplication.java
@@ -0,0 +1,13 @@
+package msnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceNotesApplication.class, args);
+    }
+
+}
---
+++ microservice-notes/src/test/java/msnote/MicroserviceNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package msnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
