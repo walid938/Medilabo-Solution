7782dc798b07abe5bebc5771463156d2f0c078c6
walid938 <w.seghdau@gmail.com>
2024-06-10 18:48:09 +0200

    Implement NoteRepository interface for MongoDB

---
+++ microservice-notes/src/main/java/msnote/repository/NoteRepository.java
@@ -0,0 +1,14 @@
+package msnote.repository;
+
+import msnote.model.Note;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoteRepository extends MongoRepository<Note, String> {
+
+    public List<Note> findByPatientId(int patientId);
+
+}
