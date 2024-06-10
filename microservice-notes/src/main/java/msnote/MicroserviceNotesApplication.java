48b45a7ba84a90a25cf20180118ef994a4240a34
walid938 <w.seghdau@gmail.com>
2024-06-10 18:55:15 +0200

    Add HTML pages for note management in the user interface

--- microservice-notes/src/main/java/msnote/MicroserviceNotesApplication.java
+++ microservice-notes/src/main/java/msnote/MicroserviceNotesApplication.java
@@ -2,6 +2,8 @@ package msnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 public class MicroserviceNotesApplication {
