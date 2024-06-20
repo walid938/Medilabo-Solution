078792c2f82d7333e1e706e03f3c2d35303c715d
walid938 <w.seghdau@gmail.com>
2024-06-20 10:33:18 +0200

    create DiabetesRiskProxy for communication with risk service

---
+++ microservice-clientui/src/main/java/msclientui/proxies/DiabeteRiskProxy.java
@@ -0,0 +1,18 @@
+package msclientui.proxies;
+
+
+import msclientui.beans.DiabeteRiskLevelBeans;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "microservice-gateway",
+        url = "http://localhost:8181"
+)
+public interface DiabeteRiskProxy {
+
+    @GetMapping(value = "api/diabeterisk/diabete-risk/{id}")
+    DiabeteRiskLevelBeans calculateDiabetesRisk(@PathVariable("id") int patientId);
+}
+
